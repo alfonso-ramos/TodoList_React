870e58d0bb27f2cd37d83df751eb3e2e6fe2648c
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-09-20 09:04:54 -0600

    [ADD] Manejo del estado para el ToDo search

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,17 +1,26 @@
+import  React  from 'react';
 import '../styles/components/TodoSearch.css'
 
 const TodoSearch = () => {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setSearchValue(event.target.value)
   }
 
-  return (
-    <input
-      className="TodoSearch"
-      placeholder="Escribe una tarea"
-      onChange={onSearchValueChange}
-    />
-  )
+  return [
+    <>
+      <input
+        className="TodoSearch"
+        placeholder="Escribe una tarea"
+        value={searchValue}
+        onChange={onSearchValueChange}
+      />
+      <p>{searchValue}</p>
+    </>
+  ]
 }
 
 export default TodoSearch
