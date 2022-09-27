8e0a2015eb5a4fe3dca332f29f980d57dc1e272a
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-09-26 23:14:58 -0600

    [ADD] Persistencia de datos en LocalStorage

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,27 @@
 import React from "react"
 import { AppUI } from "./AppUI"
 
-const defaultTodos = [
-  {text: "Tarea1", completed: false},
-  {text: "comprar mandado", completed: false},
-  {text: "ir a la universidad", completed: false},
-  {text: "Terminar el curso de introduccion a react", completed: false},
-]
+// const defaultTodos = [
+//   {text: "Tarea1", completed: false},
+//   {text: "comprar mandado", completed: false},
+//   {text: "ir a la universidad", completed: false},
+//   {text: "Terminar el curso de introduccion a react", completed: false},
+// ]
+
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos)
+
+  const localStorageTodos = localStorage.getItem('TODOS_V1')
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos)
 
   const [searchValue, setSearchValue] = React.useState('')
 
@@ -29,19 +41,25 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos)
+    localStorage.setItem('TODOS_V1' , stringifiedTodos)
+    setTodos(newTodos)
+  }
+
 // Metodo para eliminar y terminar tareas
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text)
     const newTodos = [...todos]
     newTodos[todoIndex].completed = true
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text)
     const newTodos = [...todos]
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   return (
